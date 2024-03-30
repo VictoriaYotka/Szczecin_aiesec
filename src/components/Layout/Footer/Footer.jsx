d519791ac8345c77596c4fbd6b91bcd081f3e4c3
VictoriaYotka <victoria.borisyuk@gmail.com>
2024-03-30 23:15:52 +0200

    add footer

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,5 +1,30 @@
+import css from "./Footer.module.scss";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={css.footer}>
+      <h4 className={css.heading}>Contact details:</h4>
+      <ul>
+        <li className={css.item}>
+          <h6 className={css.subheading}>EB email</h6>
+          <a href="mailto:aiesecinszczecin@gmail.com" className={css.link}>
+            aiesecinszczecin@gmail.com
+          </a>
+        </li>
+        <li className={css.item}>
+          <h6 className={css.subheading}>LC Tg chat</h6>
+          <a
+            href="https://bit.ly/AIESEC_SZCZECIN"
+            target="_blanc"
+            rel="noopener noreferrer nofollow"
+            className={css.link}
+          >
+            https://bit.ly/AIESEC_SZCZECIN
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
